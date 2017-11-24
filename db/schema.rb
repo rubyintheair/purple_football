632bb5de199560d7eebe572369fa89e442f530ee
Quy <quy.nguyenngoctp@gmail.com>
2017-11-24 22:24:11 +0700

    delete all things related to BelongTeam and Team

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124145021) do
+ActiveRecord::Schema.define(version: 20171124151407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "belong_teams", force: :cascade do |t|
+    t.integer "team_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
