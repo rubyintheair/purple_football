7e389d142e24bfa284195dbb924ae713c052befb
Quy <quy.nguyenngoctp@gmail.com>
2017-11-24 17:11:41 +0700

    method create for User, form for User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create 
+    @user = User.new(users_params)
+    if @user.save 
+      redirect_to root_path
+    else 
+      redirect_to new_user_path
+    end 
+  end 
+
+  def users_params
+    params.require(:user).permit(:first_name, :last_name)
+  end 
+end
