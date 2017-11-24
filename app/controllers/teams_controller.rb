f41ad99651dc5aea1db5bfb745f3d9e54972c4b7
Quy <quy.nguyenngoctp@gmail.com>
2017-11-24 22:06:13 +0700

    method create of teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,21 @@
 class TeamsController < ApplicationController
+  def index 
+  end 
+
+  def new 
+  end 
+
+  def create 
+    @team = Team.new(team_params)
+    if @team.save 
+      redirect_to root_path
+    else 
+      redirect_to new_team_path
+    end 
+  end 
+
+  def team_params
+    params.require(:team).permit(:name)
+  end 
+
 end
